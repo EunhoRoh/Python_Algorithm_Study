9117e047ac6ae349aaeec74ede4c1061e5d8f241
EunhoRoh <deu04189@gmail.com>
2025-02-22 19:59:19 +0900

    feat: BJ_10808 completed

--- array_basic_algo/BJ_10808.py
+++ array_basic_algo/BJ_10808.py
@@ -1,11 +1,12 @@
-S= input("알파벳을 입력하시오.")
 # 알파벳 각각의 갯수를 어떻게 알까?
 #65는 알파벳 a이다.
 # a b c d e f g h i j k l m n o p q r s t u v w x y z
 #총 26개
 # 65부터 90까지인가..?
-alpha=[0,]
+S= input("알파벳을 입력하시오.").lower()
+
+alpha=[0] *26
 for c in S:
-    alpha[int(c)-65]=alpha[int(c)-65]+1
+    alpha[ord(c)-97] +=1
     
-print(int(alpha))
+print(alpha)
